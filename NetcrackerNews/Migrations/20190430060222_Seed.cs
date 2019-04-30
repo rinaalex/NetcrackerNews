e128bd144a798529751e104c504d6a31a465c059
Katherine Burova <katherineburova@gmail.com>
2019-04-30 09:21:42 +0300

    Добавлена миграция БД с начальными данными

---
+++ NetcrackerNews/Migrations/20190430060222_Seed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NetcrackerNews.Migrations
+{
+    public partial class Seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Articles",
+                columns: new[] { "ArticleId", "Title", "Text", "TimeStamp" },
+                values: new object[] { 1, "Welcome", @"Каждая новость состоит из заголовка длиной 100 символов и текста (длина не ограничена), оба поля обязательные. 
+                        На ленте новостей должен отображаться не полный текст новости, а только первые 200 символов с кнопкой 'читать полностью',
+                        которая разворачивает текст целиком.", new DateTime(2019, 4, 30, 9, 2, 22, 459, DateTimeKind.Local) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Articles",
+                keyColumn: "ArticleId",
+                keyValue: 2);
+        }
+    }
+}
--- NetcrackerNews/Models/NewsContext.cs
+++ NetcrackerNews/Models/NewsContext.cs
@@ -12,6 +12,19 @@ namespace NetcrackerNews.Models
 
         public NewsContext(DbContextOptions<NewsContext> options) : base(options)
         {
+            
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Article>().HasData(new Article
+            {
+                ArticleId = 1,
+                Title = "Welcome",
+                Text = @"Каждая новость состоит из заголовка длиной 100 символов и текста (длина не ограничена), оба поля обязательные. 
+                        На ленте новостей должен отображаться не полный текст новости, а только первые 200 символов с кнопкой 'читать полностью',
+                        которая разворачивает текст целиком.",
+                TimeStamp = DateTime.Now
+            })
+;        }
     }
 }
